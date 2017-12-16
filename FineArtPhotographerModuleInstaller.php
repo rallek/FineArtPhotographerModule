1f6caf37547c24d2035f2d9b7e31c8d38abd2f3f
rallek <ralf@familie-koester.de>
2017-12-16 13:42:04 +0100

    installer update

--- FineArtPhotographerModuleInstaller.php
+++ FineArtPhotographerModuleInstaller.php
@@ -20,4 +20,35 @@ use RK\FineArtPhotographerModule\Base\AbstractFineArtPhotographerModuleInstaller
 class FineArtPhotographerModuleInstaller extends AbstractFineArtPhotographerModuleInstaller
 {
     // feel free to extend the installer here
+	
+public function install()
+
+{
+	
+
+    $result = parent::install();
+
+    if (true === $result) {
+
+        // eigene Werte setzen
+        // set up all our vars with initial values
+        // $this->setVar('albumEntriesPerPage', 10);
+        // $this->setVar('linkOwnAlbumsOnAccountPage', true);
+        // $this->setVar('albumItemEntriesPerPage', 10);
+        // $this->setVar('linkOwnAlbumItemsOnAccountPage', true);
+        // $this->setVar('enableShrinkingForAlbumItemImage', false);
+        $this->setVar('shrinkWidthAlbumItemImage', 3600);
+        $this->setVar('shrinkHeightAlbumItemImage', 1200);
+        $this->setVar('thumbnailModeAlbumItemImage', 'inset');
+        $this->setVar('thumbnailWidthAlbumItemImageView', 150);
+        $this->setVar('thumbnailHeightAlbumItemImageView', 50);
+        $this->setVar('thumbnailWidthAlbumItemImageDisplay', 900);
+        $this->setVar('thumbnailHeightAlbumItemImageDisplay', 300);
+        $this->setVar('thumbnailWidthAlbumItemImageEdit', 480);
+        $this->setVar('thumbnailHeightAlbumItemImageEdit', 180);
+        // $this->setVar('enabledFinderTypes', 'album###albumItem');
+		
+    }
+
+    return $result;
 }
